126d3c7fef700fbd81ac797fba7ec36cc26b52d4
Akshay Viswanathan <akshayviswa9876@gmail.com>
2024-06-11 20:42:58 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import NavBar from './components/NavBar';
 import News from './components/News';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoadingBar from 'react-top-loading-bar';
 
 const App = () => {
